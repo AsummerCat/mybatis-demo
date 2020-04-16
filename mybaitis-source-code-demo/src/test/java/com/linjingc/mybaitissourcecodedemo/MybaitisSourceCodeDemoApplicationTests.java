92c3b062ab5b63d42abeab96f0f3f8b7a43a74c7
chenxiaocehng <583188551@qq.com>
2020-04-16 16:36:19 +0800

    集成了一个自定义的自动装配

--- mybaitis-source-code-demo/src/test/java/com/linjingc/mybaitissourcecodedemo/MybaitisSourceCodeDemoApplicationTests.java
+++ mybaitis-source-code-demo/src/test/java/com/linjingc/mybaitissourcecodedemo/MybaitisSourceCodeDemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.linjingc.mybaitissourcecodedemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MybaitisSourceCodeDemoApplicationTests {
 
+	@Autowired
+	private com.linjingc.hello.service.HelloService helloService;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(helloService.heelloWord());
 	}
 
 }
